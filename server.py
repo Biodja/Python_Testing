11595f209b1fc35442b8f31c6a61af6b3aefa39b
Biodja <danielsenavon@gmail.com>
2022-12-04 04:44:07 +0100

    bugg email

--- server.py
+++ server.py
@@ -1,47 +1,38 @@
-import json
 from flask import Flask,render_template,request,redirect,flash,url_for
-
-
-def loadClubs():
-    with open('clubs.json') as c:
-         listOfClubs = json.load(c)['clubs']
-         return listOfClubs
-
-
-def loadCompetitions():
-    with open('competitions.json') as comps:
-         listOfCompetitions = json.load(comps)['competitions']
-         return listOfCompetitions
+import helper1 as HELPER
 
 
 app = Flask(__name__)
 app.secret_key = 'something_special'
 
-competitions = loadCompetitions()
-clubs = loadClubs()
-
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/showSummary',methods=['POST'])
-def showSummary():
-    club = [club for club in clubs if club['email'] == request.form['email']][0]
-    return render_template('welcome.html',club=club,competitions=competitions)
 
+@app.route("/showSummary", methods=["POST"])
+def show_summary():
+    selected_club = HELPER.get_club_by_mail(mail=request.form["email"])
+    if selected_club is None:
+        print("pas trouvé !")
+        flash("Sorry, that email wasn't found !!")
+        return render_template("index.html")
 
+    
 
+    if selected_club :
 
+        HELPER.USER_CLUB = selected_club
+        print("trouvé !")
+        return render_template(
+            "welcome.html",
+            club=HELPER.USER_CLUB,
+            competitions=HELPER.COMPETITIONS,
+        )
 
-@app.route('/book/<competition>/<club>')
-def book(competition,club):
-    foundClub = [c for c in clubs if c['name'] == club][0]
-    foundCompetition = [c for c in competitions if c['name'] == competition][0]
-    if foundClub and foundCompetition:
-        return render_template('booking.html',club=foundClub,competition=foundCompetition)
-    else:
-        flash("Something went wrong-please try again")
-        return render_template('welcome.html', club=club, competitions=competitions)
+    flash("Email address not found")
+    return render_template("index.html")
+
 
 
 @app.route('/purchasePlaces',methods=['POST'])
